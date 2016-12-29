c0741a0b241ca267afaed0417b253e8cd712d307
Pivotal <champ@honeywell.com>
2016-12-29 16:39:27 -0500

    Adding a few more test cases.

--- board.go
+++ board.go
@@ -1,5 +1,11 @@
 package battleship
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
 type Board struct {
 	Width int
 }
@@ -10,6 +16,46 @@ func (b *Board) Place(ship *Ship, r LocationRange) error {
 
 type LocationRange struct {
 	start, end string
+	Length     int
+	locations  []Location
+}
+
+func NewLocationRange(start, end string) (LocationRange, error) {
+	err := validateLocationCode(start)
+	if err != nil {
+		return LocationRange{}, err
+	}
+
+	err = validateLocationCode(end)
+	if err != nil {
+		return LocationRange{}, err
+	}
+
+	return LocationRange{}, nil
+}
+
+func validateLocationCode(code string) error {
+	if len(code) < 2 {
+		return errors.New("location code must be at least 2 characters long")
+	}
+
+	row := code[0:1]
+
+	if !strings.Contains("ABCDEFGHIJKLMNOPQRSTUVWXYZ", row) {
+		return errors.New("location code must start with a letter")
+	}
+
+	column := code[1:]
+
+	if _, err := strconv.Atoi(column); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type Location struct {
+	Row, Column string
 }
 
 func (l *LocationRange) IsValidFor(b Board, s *Ship) (bool, error) {
--- board_test.go
+++ board_test.go
@@ -1,11 +1,12 @@
 package battleship
 
 import "testing"
+import "fmt"
 
 func TestShipPlacementInvalidLocationRange(t *testing.T) {
 	board := Board{4}
 	cruiser := NewCruiser()
-	r := LocationRange{"A4", "A1"}
+	r, _ := NewLocationRange("A4", "A1")
 	err := board.Place(cruiser, r)
 
 	if err == nil {
@@ -16,15 +17,45 @@ func TestShipPlacementInvalidLocationRange(t *testing.T) {
 func TestLocationRange(t *testing.T) {
 	board := Board{4}
 	cruiser := NewCruiser()
-	r := LocationRange{"A4", "A1"}
 
-	valid, err := r.IsValidFor(board, cruiser)
-
-	if valid {
-		t.Fatal("expected invalid location range for A4:A1")
+	testCases := []struct {
+		start, end  string
+		validity    bool
+		expectError bool
+	}{
+		{"A1", "A3", true, false},
+		{"A3", "A1", false, true},
+		{"A", "A1", false, true},
 	}
 
-	if err == nil {
-		t.Fatal("expected location range validation failure, got:", err)
+	for _, testCase := range testCases {
+		t.Run(fmt.Sprintf("LocationRange(%s:%s)", testCase.start, testCase.end), func(t *testing.T) {
+			r, err := NewLocationRange(testCase.start, testCase.end)
+
+			if err != nil {
+				if !testCase.expectError {
+					t.Fatal("unexpected location range validation failure, got:", err)
+				} else {
+					return
+				}
+			} else if testCase.expectError {
+				t.Fatal("expected location range validation failure but did not get any error")
+			}
+
+			valid, err := r.IsValidFor(board, cruiser)
+
+			if valid != testCase.validity {
+				t.Fatal("expected valid =", testCase.validity, "got:", valid,
+					"for location range for", fmt.Sprintf("%s:%s", testCase.start, testCase.end))
+			}
+
+			if err != nil {
+				if !testCase.expectError {
+					t.Fatal("unexpected location range validation failure, got:", err)
+				}
+			} else if testCase.expectError {
+				t.Fatal("expected location range validation failure but did not get any error")
+			}
+		})
 	}
 }
