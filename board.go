1143a471bfaec7a4eeb7c07013892029e6439fa6
Will <wmadisonDev@GMail.com>
2016-12-29 20:03:12 -0500

    Adding some additional test cases for handling the placement of ships on a Battleship board.

--- board.go
+++ board.go
@@ -2,62 +2,167 @@ package battleship
 
 import (
 	"errors"
-	"strconv"
 	"strings"
+	"strconv"
+	"fmt"
+	"unicode/utf8"
 )
 
 type Board struct {
 	Width int
+	occupants map[Location]*Ship
 }
 
 func (b *Board) Place(ship *Ship, r LocationRange) error {
+	if _, err := r.IsValidFor(*b, ship); err != nil {
+		return err
+	}
+
+	for _, l := range r.Locations {
+		if b.IsOccupied(l) {
+			return errors.New("location " + string(l) + "is already occupied.")
+		}
+	}
+
+	if b.occupants == nil {
+		b.occupants = map[Location]*Ship{}
+	}
+
+	for _, l := range r.Locations {
+		b.occupants[l] = ship
+	}
+
 	return nil
 }
 
+func (b *Board) IsOccupied(l Location) bool {
+	_, ok := b.occupants[l]
+	return ok
+}
+
 type LocationRange struct {
-	start, end string
 	Length     int
-	locations  []Location
+	Locations  []Location
+}
+
+func (l *LocationRange) IsValidFor(b Board, s *Ship) (bool, error) {
+	if l.Length > b.Width {
+		return false, errors.New("range cannot be larger than the board")
+	}
+
+	if l.Length != s.Length {
+		return false, errors.New("range must be equal to the length of the ship")
+	}
+
+	return true, nil
 }
 
 func NewLocationRange(start, end string) (LocationRange, error) {
-	err := validateLocationCode(start)
+	startLocation := Location(start)
+	err := startLocation.Validate()
 	if err != nil {
 		return LocationRange{}, err
 	}
 
-	err = validateLocationCode(end)
+	endLocation := Location(end)
+	err = endLocation.Validate()
 	if err != nil {
 		return LocationRange{}, err
 	}
 
-	return LocationRange{}, nil
+	if !startLocation.IsBefore(endLocation) {
+		return LocationRange{}, errors.New("invalid range, start location must come before the end location")
+	}
+
+	if startLocation.IsDiagonalTo(endLocation) {
+		return LocationRange{}, errors.New("invalid range, start and end locations must not be diagonal to one another")
+	}
+
+	var length int
+
+	if startLocation.InSameColumn(endLocation) {
+		startingRune, _ := utf8.DecodeRuneInString(startLocation.Row())
+		endingRune, _ := utf8.DecodeRuneInString(endLocation.Row())
+
+		length = int(endingRune - startingRune) + 1
+	} else if startLocation.InSameRow(endLocation) {
+		length = endLocation.Column() - startLocation.Column() + 1
+	}
+
+	locations := []Location{startLocation}
+
+	locations = append(locations, locationsInRange(startLocation, endLocation)...)
+	locations = append(locations, endLocation)
+
+	if length != len(locations) {
+		return LocationRange{}, errors.New("invalid range, inappropriate number of locations for the determined length: length=" + strconv.Itoa(length) + " numLocations=" + strconv.Itoa(len(locations)) )
+	}
+
+	return LocationRange{length, locations}, nil
 }
 
-func validateLocationCode(code string) error {
-	if len(code) < 2 {
+func locationsInRange(start, end Location) []Location {
+	intermediates := []Location{}
+
+	if start.InSameColumn(end) {
+		startingRune, _ := utf8.DecodeRuneInString(start.Row())
+		endingRune, _ := utf8.DecodeRuneInString(end.Row())
+
+		for i := startingRune+1; i < endingRune; i++ {
+			intermediates = append(intermediates, Location(fmt.Sprintf("%c%d", i, start.Column())))
+		}
+	} else if start.InSameRow(end) {
+		for i := start.Column()+1; i < end.Column(); i++ {
+			intermediates = append(intermediates, Location(fmt.Sprintf("%s%d", start.Row(), i)))
+		}
+	}
+
+	return intermediates
+}
+
+type Location string
+
+func (l Location) Row() string {
+	return string(l[0:1])
+}
+
+func (l Location) Column() int {
+	column, _ := strconv.Atoi(string(l[1:]))
+	return column
+}
+
+func (l Location) Validate() error {
+	if len(l) < 2 {
 		return errors.New("location code must be at least 2 characters long")
 	}
 
-	row := code[0:1]
+	row := string(l[0:1])
 
 	if !strings.Contains("ABCDEFGHIJKLMNOPQRSTUVWXYZ", row) {
 		return errors.New("location code must start with a letter")
 	}
 
-	column := code[1:]
+	column := l[1:]
 
-	if _, err := strconv.Atoi(column); err != nil {
+	if _, err := strconv.Atoi(string(column)); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-type Location struct {
-	Row, Column string
+func (l Location) IsBefore(other Location) bool {
+	return l.Row() < other.Row() || l.Column() < other.Column()
 }
 
-func (l *LocationRange) IsValidFor(b Board, s *Ship) (bool, error) {
-	return false, nil
+func (l Location) IsDiagonalTo(other Location) bool {
+	return l.Row() != other.Row() && l.Column() != other.Column()
+}
+
+func (l Location) InSameColumn(other Location) bool {
+	return l.Column() == other.Column()
+}
+
+func (l Location) InSameRow(other Location) bool {
+	return l.Row() == other.Row()
 }
--- board_test.go
+++ board_test.go
@@ -4,7 +4,7 @@ import "testing"
 import "fmt"
 
 func TestShipPlacementInvalidLocationRange(t *testing.T) {
-	board := Board{4}
+	board := Board{Width: 4}
 	cruiser := NewCruiser()
 	r, _ := NewLocationRange("A4", "A1")
 	err := board.Place(cruiser, r)
@@ -14,18 +14,79 @@ func TestShipPlacementInvalidLocationRange(t *testing.T) {
 	}
 }
 
+func TestShipPlacementValidLocation(t *testing.T) {
+	board := Board{Width: 4}
+	cruiser := NewCruiser()
+	r, _ := NewLocationRange("A1", "A3")
+	err := board.Place(cruiser, r)
+
+	if err != nil {
+		t.Fatal("unexpected placement failure, got:", err)
+	}
+}
+
+func TestShipPlacementOverlappingBoats(t *testing.T) {
+	board := Board{Width: 4}
+	cruiser := NewCruiser()
+	r, _ := NewLocationRange("A1", "A3")
+	err := board.Place(cruiser, r)
+
+	if err != nil {
+		t.Fatal("unexpected placement failure, got:", err)
+	}
+
+	cruiser = NewCruiser()
+	r, _ = NewLocationRange("A2", "C2")
+	err = board.Place(cruiser, r)
+
+	if err == nil {
+		t.Fatal("expected placement failure, got:", err)
+	}
+}
+
+func TestShipPlacementNonOverlappingBoats(t *testing.T) {
+	board := Board{Width: 4}
+	cruiser := NewCruiser()
+	r, _ := NewLocationRange("A1", "A3")
+	err := board.Place(cruiser, r)
+
+	if err != nil {
+		t.Fatal("unexpected placement failure, got:", err)
+	}
+
+	cruiser = NewCruiser()
+	r, _ = NewLocationRange("A4", "C4")
+	err = board.Place(cruiser, r)
+
+	if err != nil {
+		t.Fatal("unexpected placement failure, got:", err)
+	}
+
+	for _, loc := range []Location{"A4", "B4", "C4"} {
+		if !board.IsOccupied(loc) {
+			t.Fatal("expected location " + string(loc) + " to be occupied")
+		}
+	}
+}
+
 func TestLocationRange(t *testing.T) {
-	board := Board{4}
+	board := Board{Width: 4}
 	cruiser := NewCruiser()
 
 	testCases := []struct {
 		start, end  string
+		expectedLength int
 		validity    bool
-		expectError bool
+		expectRangeError bool
+		expectValidityError bool
 	}{
-		{"A1", "A3", true, false},
-		{"A3", "A1", false, true},
-		{"A", "A1", false, true},
+		{"A1", "A3", 3, true, false, false},
+		{"A1", "C1", 3, true, false, false},
+		{"A3", "A1", 0, false, true, true},
+		{"A", "A1", 0, false, true, true},
+		{"AA", "A1", 0, false, true, true},
+		{"A1", "C4", 0, false, true, true},
+		{"A1", "A17", 17, false, false, true},
 	}
 
 	for _, testCase := range testCases {
@@ -33,13 +94,17 @@ func TestLocationRange(t *testing.T) {
 			r, err := NewLocationRange(testCase.start, testCase.end)
 
 			if err != nil {
-				if !testCase.expectError {
+				if !testCase.expectRangeError {
 					t.Fatal("unexpected location range validation failure, got:", err)
 				} else {
 					return
 				}
-			} else if testCase.expectError {
+			} else if testCase.expectRangeError {
 				t.Fatal("expected location range validation failure but did not get any error")
+			}
+
+			if r.Length != testCase.expectedLength {
+				t.Fatal("expected length of", testCase.expectedLength, "got:", r.Length)
 			}
 
 			valid, err := r.IsValidFor(board, cruiser)
@@ -50,11 +115,11 @@ func TestLocationRange(t *testing.T) {
 			}
 
 			if err != nil {
-				if !testCase.expectError {
-					t.Fatal("unexpected location range validation failure, got:", err)
+				if !testCase.expectValidityError {
+					t.Fatal("unexpected location range validity failure, got:", err)
 				}
-			} else if testCase.expectError {
-				t.Fatal("expected location range validation failure but did not get any error")
+			} else if testCase.expectValidityError {
+				t.Fatal("expected location range validity failure but did not get any error")
 			}
 		})
 	}
